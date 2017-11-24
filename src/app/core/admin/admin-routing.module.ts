b236426d5ecca77b6666f284b06a4d0faaa4c17f
hzxc <hzxc@LK1028>
2017-11-24 14:37:42 +0800

    '1'

--- src/app/core/admin/admin-routing.module.ts
+++ src/app/core/admin/admin-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
+import { TenantsComponent } from './tenants/tenants.component';
 
 const routes: Routes = [
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent },
+  { path: 'tenants', component: TenantsComponent }
 ];
 
 @NgModule({
