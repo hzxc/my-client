c610b4509d0c56115f94499cf651fc06caa69e15
hzxc <hzxc@LK1028>
2017-12-22 18:43:40 +0800

    1

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,10 +1,10 @@
 /* SystemJS module definition */
 ///<reference path="../node_modules/@types/jquery/index.d.ts"/>
 ///<reference path="../node_modules/@types/jstree/index.d.ts"/>
-// ///<reference path="../node_modules/@types/signalr/index.d.ts"/>
+///<reference path="../node_modules/@types/signalr/index.d.ts"/>
 ///<reference path="./app/abp/resources/scripts/abp.d.ts"/>
 ///<reference path="./app/abp/resources/scripts/libs/abp.jquery.d.ts"/>
-// ///<reference path="./app/abp/resources/scripts/libs/abp.signalr.d.ts"/>
+///<reference path="./app/abp/resources/scripts/libs/abp.signalr.d.ts"/>
 ///<reference path="../node_modules/moment/moment.d.ts"/>
 ///<reference path="../node_modules/@types/moment-timezone/index.d.ts"/>
 ///<reference path="../node_modules/@types/toastr/index.d.ts"/>
