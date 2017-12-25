ee960932e22c1fa113d1394055894e9ada8989c1
Home.hzxc <a0@live.cn>
2017-12-25 22:10:53 +0800

    201712251

--- src/app/core/admin/notifications/notifications.component.ts
+++ src/app/core/admin/notifications/notifications.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit, Injector } from '@angular/core';
+import { Component, OnInit, Injector, ViewChild } from '@angular/core';
 import { AppComponentBase } from '../../../shared/common/app-component-base';
+import { DataSource, CollectionViewer } from '@angular/cdk/collections';
+import { UserNotification, NotificationServiceProxy } from '../../../shared/service-proxies/service-proxies';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { MatPaginator, MatSort, MatSnackBar, MatDialog } from '@angular/material';
+import { AppUserNotificationState } from '../../../shared/AppEnums';
+import { IFormattedUserNotification, UserNotificationHelper } from '../../shared/notifications/UserNotificationHelper';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-notifications',
@@ -8,13 +16,115 @@ import { AppComponentBase } from '../../../shared/common/app-component-base';
 })
 export class NotificationsComponent extends AppComponentBase implements OnInit {
 
+
+  displayedColumns = [
+    'actions',
+    'text',
+    'creationTime',
+  ];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  private dataSource: UserNotificationDataSource;
+  private readStateFilter: string;
+
   constructor(
-    injector: Injector
+    injector: Injector,
+    private _notificationService: NotificationServiceProxy,
+    private _userNotificationHelper: UserNotificationHelper,
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog
   ) {
     super(injector);
   }
 
   ngOnInit() {
+    this.dataSource = new UserNotificationDataSource(
+      this.readStateFilter,
+      this.paginator,
+      this.sort,
+      this.snackBar,
+      this._notificationService,
+      this._userNotificationHelper
+    );
   }
 
+  fromNow(date: moment.Moment): string {
+    return moment(date).fromNow();
+  }
+
+  truncateString(text: any, length: number): string {
+    return abp.utils.truncateStringWithPostfix(text, length);
+  }
+}
+
+export class UserNotificationDataSource extends DataSource<UserNotification> {
+
+
+  private isLoadingResults = true;
+
+  reloadBehavior = new BehaviorSubject(false);
+  get reload(): boolean { return this.reloadBehavior.value; }
+  set reload(reloadActive: boolean) { this.reloadBehavior.next(reloadActive); }
+  public reloadTable() {
+    this.reloadBehavior.next(true);
+  }
+
+  constructor(
+    private readStateFilter: string,
+    private paginator: MatPaginator,
+    private sort: MatSort,
+    private snackBar: MatSnackBar,
+    private _notificationService: NotificationServiceProxy,
+    private _userNotificationHelper: UserNotificationHelper
+  ) {
+    super();
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<UserNotification[]> {
+    const displayDataChanges = [
+      this.sort.sortChange,
+      this.paginator.page,
+      this.reloadBehavior,
+    ];
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    if (this.sort.direction === '') {
+      this.sort.direction = 'asc';
+    }
+    return Observable
+      .merge(...displayDataChanges)
+      .startWith(null)
+      .switchMap(() => {
+        this.isLoadingResults = true;
+        return this._notificationService.getUserNotifications(
+          this.readStateFilter === 'ALL' ? undefined : AppUserNotificationState.Unread,
+          this.paginator.pageSize,
+          this.paginator.pageIndex * this.paginator.pageSize);
+      }).map(result => {
+        this.isLoadingResults = false;
+        this.paginator.length = result.totalCount;
+        if (!result.items || result.items.length === 0) {
+          this.snackBar.open('NoData', 'Close', {
+            duration: 2000,
+          });
+        }
+        return this.formatNotifications(result.items);
+      });
+  }
+
+  formatNotifications(records: any[]): any[] {
+    const formattedRecords = [];
+    for (const record of records) {
+      record.formattedNotification = this.formatRecord(record);
+      formattedRecords.push(record);
+    }
+    return formattedRecords;
+  }
+
+  formatRecord(record: any): IFormattedUserNotification {
+    return this._userNotificationHelper.format(record, false);
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+  }
 }
